9834d51ee2a8972bd76f8e8450a4316a892c53fa
Emilio <emilio@emilio-Lenovo-IdeaPad-Y400.(none)>
2014-02-05 13:23:03 +0100

    Handling UDP and TCP
    This software handles both UDP and TCP connections

--- Network/network.go
+++ Network/network.go
@@ -4,6 +4,8 @@ import(
 	"fmt"
 	"net"
 	"os"
+	"time"    //This file is for the sleep time
+	"runtime" //Used for printing the line on the console
 )
 // the listener should be given some channels to put the data
 func Listener(){
@@ -14,32 +16,65 @@ func Listener(){
 
 	udpAddr1,err := net.ResolveUDPAddr("udp4",PortStatus)
 	check(err)
-	udpAddr2,err := net.ResolveUDPAddr("udp4",PortCmd)
+	udpAddr2,err := net.ResolveTCPAddr("tcp4",PortCmd)
 	check(err)
 
-	ConnStatus,err := net.ListenUDP("udp",udpAddr1)
-	check(err)
-	ConnCmd,err := net.ListenUDP("udp",udpAddr2)
-	check(err)
+	_,file,line,_ := runtime.Caller(0) 
+	fmt.Println(file, line)
 
-	for{
-		//fmt.Println("for")
-		handleConnection(ConnStatus)
-		handleConnection(ConnCmd)
+	ConnStatus,err := net.ListenUDP("udp",udpAddr1)       
+	check(err)
+	ConnCmd,err := net.ListenTCP("tcp",udpAddr2)
+	check(err)
+	
+	//Create go rutines	
+	go handleConnectionUDP(ConnStatus)
+	go handleConnectionTCP(ConnCmd)
+
+	for {
+		time.Sleep(5000*time.Millisecond)
+	    fmt.Println("for")	
 	}
+
+    _,file,line,_ = runtime.Caller(0) 
+	fmt.Println(file, line)
 
 }
 
-func handleConnection(conn *net.UDPConn){
-	var buf [1024]byte
-	_,addr,err := conn.ReadFromUDP(buf[0:])
-	check(err)
-	fmt.Printf("From: %s: %s\n",addr,buf)
+func handleConnectionUDP(conn *net.UDPConn){
+
+	var buf [1024]byte	
+    for {
+		_,addr,_ := conn.ReadFromUDP(buf[0:])
+		//check(err)	
+		fmt.Printf("From: %s: %s\n",addr,buf)	
+	}
+}
+
+func handleConnectionTCP(ConnCmd *net.TCPListener){
+	var buf [1024]byte	
+	var length int
+	var readerr error    //Error variable for detecting when a client got off
+	readerr = nil
+
+	for {		
+	    TCPconn,err := ConnCmd.Accept()	   
+	    check(err)
+	    //You will read until you detect an error on the TCP connection
+	    for readerr == nil{
+		    length,readerr = TCPconn.Read(buf[0:])
+			check(readerr)
+			if  readerr == nil && length != 0{
+				fmt.Printf("From: %d: %s\n",length,buf)
+			}
+	    }
+	    readerr = nil	    
+	    TCPconn.Close()
+	}
 }
 
 func check(err error){
 	if err != nil{
-		fmt.Fprintf(os.Stderr,"Error: %s",err.Error())
+		fmt.Fprintf(os.Stderr,"Error: %s\n",err.Error())
 	}
 }
-

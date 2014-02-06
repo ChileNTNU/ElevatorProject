77547380fa763d968033f9a8fa9d9e4b5194fbbf
massad77 <massad.mw@web.de>
2014-02-06 14:02:58 +0100

    ClientTest modified for TCP
    TCP client was added but the TCP connection does not work convenient yet. When sending messages of fixed size it works

--- ClientTest.go
+++ ClientTest.go
@@ -5,64 +5,90 @@ import (
 	"time"
 	"net"
 	"os"
+	"io"
+//	"bufio"
 )
 
 const REMOTEIP = "78.91.18.88"
 const LOCALIP = "78.91.7.106"
 const PORTTCP = ":20020"
 const PORTUDP = ":20019"
-//	var portudpl string = ":20019"
-
 
 func main(){
 	var err error
-	var RemoteAddr *net.UDPAddr
-	var LocalAddr *net.UDPAddr
-	var conn *net.UDPConn
+	var RemoteAddrUDP *net.UDPAddr
+	var LocalAddrUDP *net.UDPAddr
+	var UDPconn *net.UDPConn
+	var conn net.Conn
 	fmt.Println("I'm a client.");
 
-	RemoteAddr,err = net.ResolveUDPAddr("udp4",REMOTEIP+PORTUDP)
-	LocalAddr,err = net.ResolveUDPAddr("udp4",LOCALIP+PORTUDP)
+	RemoteAddrUDP,err = net.ResolveUDPAddr("udp4",REMOTEIP+PORTUDP)
+	LocalAddrUDP,err = net.ResolveUDPAddr("udp4",LOCALIP+PORTUDP)
 
-	//Make a socket for the UDP connection
+
+	//Make a socket for the connection
 	//LocalAddr used to send from(address+port)
-	conn,err = net.DialUDP("udp",LocalAddr,RemoteAddr)
+	UDPconn,err = net.DialUDP("udp",LocalAddrUDP,RemoteAddrUDP)
 	checkError(err)
-//	conn1,err := net.Dial("tcp",ip+":"+porttcp)
-//	checkError(err)
+	//net.DialTCP does not work somehow
+	conn,err = net.Dial("tcp",REMOTEIP+PORTTCP)
+	checkError(err)
 
 
 	//Listen on the same connection (socket)
-	go ListenOnUdp(conn)
+	go ListenOnUdp(UDPconn)
+	go ListenOnTcp(conn)
 
 	//write to the connections
 	for i:=0;i<5;i++{
-//		_,err = conn1.Write([]byte("Anything on TCP!"))
-//		checkError(err)
-
-		_,err = conn.Write([]byte("Anything on UDP!"))
+		_,err = conn.Write([]byte("Anything on TCP!\x00"))
 		checkError(err)
 
+		_,err = UDPconn.Write([]byte("Anything on UDP!\x00"))
+		checkError(err)
 
 	}
 	fmt.Println("Done writing")
 	time.Sleep(5000*time.Millisecond)
+	for{}
+}
+
+func ListenOnTcp(TCPconn net.Conn){
+	var buf [5]byte
+//	var msg string
+	var length int
+	var err error
+	for err == nil{
+		fmt.Println("listening TCP")
+		length,err = TCPconn.Read(buf[0:])
+//		msg,err = bufio.NewReader(TCPconn).ReadString('\x00')
+		checkError(err)
+//		fmt.Printf("(TCP)From: %s: %s\n",REMOTEIP+PORTTCP,msg)
+
+		if err == io.EOF || length == 0{
+			TCPconn.Close()
+			fmt.Println("closeed")
+			TCPconn = nil
+		}else{
+			fmt.Printf("(TCP)From: %s: %s\n",REMOTEIP+PORTTCP,buf)
+		}
+
+	}
 }
 
 
-
-
-func ListenOnUdp(conn *net.UDPConn){
+func ListenOnUdp(UDPconn *net.UDPConn){
 	var buf [1024]byte
 	var RemoteAddr *net.UDPAddr
 	var err error
 	for{
-		fmt.Println("listening")
-		_,RemoteAddr,err = conn.ReadFromUDP(buf[0:])
+//		fmt.Println("listening")
+		_,RemoteAddr,err = UDPconn.ReadFromUDP(buf[0:])
 		checkError(err)
-		fmt.Printf("From: %s: %s\n",RemoteAddr,buf)
+		fmt.Printf("(UDP)From: %s: %s\n",RemoteAddr,buf)
 	}
 }
+
 
 func checkError(err error){
 	if err != nil{

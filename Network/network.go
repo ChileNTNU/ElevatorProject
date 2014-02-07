10666fe231088680fc076e1c90a546aaa309a49c
massad77 <massad.mw@web.de>
2014-02-07 18:23:01 +0100

    Network module v0.1
    Listener and Sender for udp added. Channels to read from and write to added. Synchronous message passing does not work somehow.
    []byte encoding does not work yet

--- Network/network.go
+++ Network/network.go
@@ -4,104 +4,90 @@ import(
     "fmt"
     "net"
     "os"
+	"encoding/gob"
     "time"    //This file is for the sleep time
     "runtime" //Used for printing the line on the console
     //"bufio"   //This is for implementing a newReader for reading TCP
 )
 
-const PORT_STATUS = ":20019"   //Port for UDP
-const PORT_CMD  = ":20020"     //Port for TCP
+const PORT_STATUS = ":20019"
+const PORT_CMD  = ":20018"
 
-// the listener should be given some channels to put the data
-func Listener(){
-    
-    //IPsend := "78.91.6.254"
-    
+type Message struct{
+	IDsender string
+	IDreceiver string
+	MsgType byte
+	Size byte
+	body [16]byte
+}
+
+
+func NetworkManager(D_Input chan Message,D_Output chan Message,R_Input chan Message,R_Output chan Message,){
+
     fmt.Println("NetworkManager started")
 
-    localudpAddr,err := net.ResolveUDPAddr("udp4",PORT_STATUS)
+    LocalAddrStatus,err := net.ResolveUDPAddr("udp4",PORT_STATUS)
     check(err)
-    localtcpAddr,err := net.ResolveTCPAddr("tcp4",PORT_CMD)
+	RemoteAddrStatus,err := net.ResolveUDPAddr("udp4","78.91.19.160"+PORT_STATUS)
+    check(err)
+    LocalAddrCmd,err := net.ResolveUDPAddr("udp4",PORT_CMD)
+    check(err)
+	RemoteAddrCmd,err := net.ResolveUDPAddr("udp4","78.91.19.160"+PORT_CMD)
     check(err)
 
-    _,file,line,_ := runtime.Caller(0) 
+    _,file,line,_ := runtime.Caller(0)
     fmt.Println(file, line)
 
-    ConnStatus,err := net.ListenUDP("udp",localudpAddr)
-    check(err)    
-    ConnCmd,err := net.ListenTCP("tcp",localtcpAddr)
-    check(err)
-    
-    //Create go rutines 
-    go handleConnectionUDP(ConnStatus)
-    go handleConnectionTCP(ConnCmd)
+	//Make a socket for the connection
+	//Listen and Send happens on the same connection (also same port)
+	ConnStatus,err := net.DialUDP("udp",LocalAddrStatus,RemoteAddrStatus)
+	check(err)
+	ConnCmd,err := net.DialUDP("udp",LocalAddrCmd,RemoteAddrCmd)
+	check(err)
 
+
+    //Create go routines 
+	go Listener(ConnStatus,R_Output)
+	go Listener(ConnCmd,D_Output)
+	go Sender(ConnStatus,R_Input)
+//	go Sender(ConnCmd,D_Input)
     for {
         time.Sleep(5000*time.Millisecond)
-        //fmt.Println("For inside Network")
+        fmt.Println("For inside Network")
     }
 
-    _,file,line,_ = runtime.Caller(0) 
+    _,file,line,_ = runtime.Caller(0)
     fmt.Println(file, line)
 
 }
 
-func handleConnectionUDP(conn *net.UDPConn){
-
-    var buf [1024]byte  
+func Listener(conn *net.UDPConn,Channel chan<- Message){
+	var MsgRecv Message
     for {
-        _,remoteAddr,err := conn.ReadFromUDP(buf[0:])
-        check(err)          
-        //fmt.Printf("From: %s: %s\n",remoteAddr,buf)
-        _,err = conn.WriteToUDP(buf[0:1],remoteAddr)
+		//Create decoder
+		dec := gob.NewDecoder(conn)
+		//Receive message on connection
+		err := dec.Decode(&MsgRecv)
         check(err)
+		fmt.Println(MsgRecv)
+		Channel <-MsgRecv
     }
 }
 
-func handleConnectionTCP(ConnCmd *net.TCPListener){
-    var buf [17]byte  
-    var length int
-    //var stringRecv string
-    var readErr error    //Error variable for detecting when a client got off
-    var writeErr error
-    readErr = nil
-
-    for {       
-        TCPconn,err := ConnCmd.Accept()    
-        check(err)
-
-        _,file,line,_ := runtime.Caller(0) 
-        fmt.Println(file, line)
-
-        //You will read until you detect an error on the TCP connection
-        for readErr == nil{
-            length,readErr = TCPconn.Read(buf[0:])            
-    //        stringRecv,readErr = bufio.NewReader(TCPconn).ReadString('\x00')
-            //bufio.NewReader(TCPconn).ReadString(byte('\x00'))
-
-              //length,writeErr = TCPconn.Write([]byte("Hola\x00"))
-              //TCPconn.Write([]byte("Hola\x00"))
-            check(readErr)
-            //---------------------
-            //fmt.Printf("TCP From: %d. %s\n",length,stringRecv)
-                //length,writeErr = TCPconn.Write([]byte("Hola\x00"))
-                //check(writeErr)            
-            //--------------------
-            
-            if  readErr == nil && length != 0{
-                fmt.Printf("TCP From: %d: %s\n",length,buf)
-                //fmt.Printf("TCP From: %d: %s\n",length,stringRecv)
-                //length,writeErr = TCPconn.Write(buf[0:3])
-                length,writeErr = TCPconn.Write([]byte("Hola\x00"))
-                check(writeErr)
-            } 
-                       
-        }
-        readErr = nil  
-        fmt.Printf("Connection closed...\n")
-        TCPconn.Close()
-    }
+func Sender(conn *net.UDPConn, Channel <-chan Message){
+	b := [16]byte {3,5}
+	MsgSend := Message{"ID1","ID2",1,2,b}
+	fmt.Println(MsgSend)
+	MsgSend =<-Channel
+	fmt.Println(MsgSend)
+	//Create encoder
+	enc := gob.NewEncoder(conn)
+	//Send encoded message on connection
+	err := enc.Encode(MsgSend)
+	check(err)
 }
+
+
 
 func check(err error){
     if err != nil{
--- main.go
+++ main.go
@@ -7,9 +7,25 @@ import(
 	"./Network"
 )
 
+
 func main(){
+	D_Input := make(chan network.Message)
+	D_Output := make(chan network.Message)
+	R_Input := make(chan network.Message)
+	R_Output := make(chan network.Message)
+
+	var testmsg network.Message
+	testmsg.IDsender = "myIP"
+	testmsg.IDreceiver = "yourIP"
+	testmsg.MsgType = 1
+	testmsg.Size = 2
+
 	fmt.Println("Hello!")
-	go network.Listener()
+//	fmt.Println(testmsg)
+	go network.NetworkManager(D_Input,D_Output,R_Input,R_Output)
+	fmt.Println("after go")
+	D_Input <- testmsg
+	fmt.Println("Msg in channel")
 
 	for{
 		fmt.Println("Doing something else")
